b634003b8dff8a18f2091a77b86ac7f3a1120ced
cullen <cullen.dolan@gmail.com>
2021-01-09 06:39:14 -0600

    trying to add user lookup function

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from user_tweets import get_user_tweets
 import json
+from get_user import convert_username_to_id
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    twitter_user_id = "3291691"
+    twitter_user_name = "chamath"
+    twitter_user_id = convert_username_to_id(twitter_user_name)
     # call a function from a different file
     returned_tweets = get_user_tweets(twitter_user_id)
-    returned_tweets = json.dumps(returned_tweets, indent=4, sort_keys=True)
     return returned_tweets
 
 if __name__ == "__main__":
--- get_user.py
+++ get_user.py
@@ -1,2 +1,33 @@
+import requests
+import json
+
 def convert_username_to_id(user_name):
-    
+    BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAACcvKQEAAAAAeh2CeFCS9U8JFAOLqW4kI3icfrg%3Dl7dxrNDY0MdfUOIAQhFmS6jEPgcIsZ5BJyEGVtWzDfs3niaqMC'
+    url = create_url(user_name)
+    headers = create_headers(BEARER_TOKEN)
+    json_response = connect_to_endpoint(url, headers)
+    #json_response = json.dumps(json_response['data']['id'])
+    print(json_response['data']['id'])
+
+
+def create_url(user_name):
+    usernames = "usernames="+user_name
+    user_fields = "user.fields=description,created_at"
+    url = "https://api.twitter.com/2/users/by?{}".format(usernames)#, user_fields) #&{}
+    return url
+
+def create_headers(BEARER_TOKEN):
+    headers = {"Authorization": "Bearer {}".format(BEARER_TOKEN)}
+    return headers
+
+
+def connect_to_endpoint(url, headers):
+    response = requests.request("GET", url, headers=headers) 
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
