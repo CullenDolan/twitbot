dd2c786f1e2d606a3028c21df566193392e559cf
cullen <cullen.dolan@gmail.com>
2021-01-09 20:30:06 -0600

    addeding the form input

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from user_tweets import get_user_tweets
 from get_user import convert_username_to_id
 
 app = Flask(__name__)
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/data', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+
+
 @app.route('/')
 def index():
-    # pick the person
-    twitter_user_name = "chamath" #'3291691'
-    # convert the username to a user id
+    twitter_username = form_data.item()
+    # pick the person, convert name to id, get the tweets
+    #twitter_user_name = "chamath"
     twitter_user_id = convert_username_to_id(twitter_user_name)
-    # call a function from a different file
     returned_tweets = get_user_tweets(twitter_user_id)
-    #print the tweets on the page
     return returned_tweets
 
 if __name__ == "__main__":
